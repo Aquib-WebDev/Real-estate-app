471734b627e69f9ff460be30beb24fb6d3c4404c
Aquib <aquib995@gmail.com>
2024-03-10 17:04:00 +0530

    Routes and Pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import {BrowserRouter,Routes, Route} from "react-router-dom"
+import Home from "./components/Home"
+import SignIn from "./components/SignIn"
+import SignUp from "./components/SignUp"
+import About from "./components/About"
+import Profile from "./components/Profile"
 
 const App = () => {
   return (
-    <div className='text-red'>App</div>
+   <BrowserRouter>
+    <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/sign-in" element = {<SignIn/>}/>
+        <Route path="/sign-up" element = {<SignUp/>}/>
+        <Route path="/about" element = {<About/>}/>
+        <Route path="/profile" element = {<Profile/>}/>
+    </Routes>
+   </BrowserRouter>
   )
 }
 
