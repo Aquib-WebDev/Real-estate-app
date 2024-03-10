ebcd980e643a809a089d391d3236bd69ed52d715
Aquib <aquib995@gmail.com>
2024-03-10 19:01:29 +0530

    Header Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignIn from "./components/SignIn"
 import SignUp from "./components/SignUp"
 import About from "./components/About"
 import Profile from "./components/Profile"
+import Header from "./components/Header"
 
 const App = () => {
   return (
    <BrowserRouter>
+   <Header/>
     <Routes>
         <Route path="/" element = {<Home/>}/>
         <Route path="/sign-in" element = {<SignIn/>}/>
