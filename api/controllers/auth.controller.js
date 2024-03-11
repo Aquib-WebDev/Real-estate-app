6b21d4fe846545f87a0bf2b2b765a4305f544ae7
Aquib <aquib995@gmail.com>
2024-03-11 13:21:50 +0530

    MiddleWare and Error for handling error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,15 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-export const signup =  async (req, res) => {
- const {userName, email, password} = req.body;
- const hashedPassword = bcryptjs.hashSync(password,10);
- const newUser = new User({userName, email, password : hashedPassword});
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res,next) => {
+  const { userName, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ userName, email, password: hashedPassword });
 
- try {
-   await newUser.save();
-   res.status(201).json("User Created Sucessfully");
-  
- } catch (error) {
-   res.status(500).json(error.message);
- }
+  try {
+    await newUser.save();
+    res.status(201).json("User Created Sucessfully");
+  } catch (error) {
+    next(error);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import authRouter from  "./routes/auth.route.js";
+import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 
 dotenv.config();
@@ -22,6 +22,15 @@ app.listen(port, () => {
   console.log("Server is running on port " + port + "!!!");
 });
 
-app.use('/api/auth',authRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-app.use('/api/user', userRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
