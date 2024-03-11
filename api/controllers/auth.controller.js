13ad948990cc64730d7152f00fd4e118e6a9104d
Aquib <aquib995@gmail.com>
2024-03-11 17:11:08 +0530

    Sign In Functionality Completed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-export const signup = async (req, res,next) => {
+import jwt from "jsonwebtoken";
+export const signup = async (req, res, next) => {
   const { userName, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ userName, email, password: hashedPassword });
@@ -11,5 +12,24 @@ export const signup = async (req, res,next) => {
     res.status(201).json("User Created Sucessfully");
   } catch (error) {
     next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found !"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credentials !"));
+    const { password : pass , ...rest} = validUser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error)
   }
 };
