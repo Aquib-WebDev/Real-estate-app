8c82c4ca7d5d94336afc9416c93c4d7f2a4bab9e
Aquib <aquib995@gmail.com>
2024-03-14 13:17:19 +0530

    Update User functionality Completed

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -3,13 +3,13 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  console.log(token);
-  if (!token) return errorHandler(401, "UnAuthorized");
+
+  if (!token) return next(errorHandler(401, "UnAuthorized"));
 
   try {
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-      if (err) return errorHandler(403, "Forbidden");
-      console.log(user);
+      if (err) return next(errorHandler(403, "Forbidden"));
+
       req.user = user;
       next();
     });
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useRef, useState } from "react";
 import {
   getDownloadURL,
@@ -7,16 +7,22 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+import {
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} from "../redux/user/userSlice";
 
 export default function () {
-  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const fileRef = useRef(null);
   const [file, setFile] = useState(undefined);
   const [filePer, setFilePer] = useState(0);
-  
+
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
- console.log(formData);
+  const[updateSucess,setUpdateSuccess]= useState(false);
 
   useEffect(() => {
     if (file) {
@@ -26,7 +32,7 @@ export default function () {
 
   const handleFileUpload = (file) => {
     const storage = getStorage(app);
-    const currentTime =  new Date().getTime() ;
+    const currentTime = new Date().getTime();
     const fileName = currentTime + file.name;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, file);
@@ -36,7 +42,6 @@ export default function () {
         const progress =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setFilePer(Math.round(progress));
-        console.log(progress);
       },
       (error) => {
         setFileUploadError(true);
@@ -49,11 +54,37 @@ export default function () {
       }
     );
   };
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  }
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold my-7 text-center">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
@@ -68,24 +99,34 @@ export default function () {
           className="cursor-pointer rounded-full w-24 h-24 mt-2 self-center object-cover"
         />
         <p className="text-sm self-center">
-        {fileUploadError ? 
-        (<span className="text-red-700">Error Uploading Image (Image should be less than 2MB)</span>)
-         :filePer >0 && filePer < 100 ? 
-         (<span className="text-green-700">{`Uploading ${filePer}`}</span>) 
-         : filePer == 100 
-         ? (<span className="text-green-700">Image Uploaded Successfully !</span>)
-         : (' ')}
-         </p>
+          {fileUploadError ? (
+            <span className="text-red-700">
+              Error Uploading Image (Image should be less than 2MB)
+            </span>
+          ) : filePer > 0 && filePer < 100 ? (
+            <span className="text-green-700">{`Uploading ${filePer}`}</span>
+          ) : filePer == 100 ? (
+            <span className="text-green-700">
+              Image Uploaded Successfully !
+            </span>
+          ) : (
+            " "
+          )}
+        </p>
         <input
           type="text"
           placeholder="username"
-          id="username"
+          id="userName"
+          defaultValue={currentUser.userName}
+          onChange={handleChange}
           className="border p-3 rounded-lg"
         />
         <input
           type="email "
           placeholder="email"
           id="email"
+          defaultValue={currentUser.email}
+          onChange={handleChange}
           className="border p-3 rounded-lg"
         />
         <input
@@ -93,16 +134,19 @@ export default function () {
           placeholder="password"
           id="password"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 p-3 rounded-lg text-white hover:opacity-90 uppercase">
-          {" "}
-          Update
+        <button disable= {loading} className="bg-slate-700 p-3 rounded-lg text-white hover:opacity-90 uppercase">
+         
+         {loading ? 'Loading...' :'Update'}
         </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+      <p className="text-green-700 mt-5">{updateSucess ? 'User has been updated Succesfully !!!' : ''}</p>
     </div>
   );
 }
