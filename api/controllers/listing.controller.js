aa512dafb0f362af24d4808455c8566c97c32c1a
Aquib <aquib995@gmail.com>
2024-03-16 15:55:44 +0530

    Create Listing functionality completed

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,10 +1,10 @@
 import Listing from "../models/listing.model.js";
 
-export const createListing = async (req,res,next)=> {
-try {
+export const createListing = async (req, res, next) => {
+  try {
     const listing = await Listing.create(req.body);
     res.status(201).json(listing);
-} catch (error) {
+  } catch (error) {
     next(error);
-}
+  }
 };
